4756daca02969888b2cae397ada0c2392f4c787e
sooncholkwon <35288758+sooncholkwon@users.noreply.github.com>
2021-07-29 13:08:34 +0900

    test
    test

---
+++ Test2Application2.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2Application2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test2Application2.class, args);
+				
+	}
+
+}
